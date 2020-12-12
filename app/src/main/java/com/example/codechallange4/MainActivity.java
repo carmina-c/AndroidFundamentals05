863567e58654446fe53e46d636d0f370e106dfe7
carmina-c <57131600+carmina-c@users.noreply.github.com>
2020-12-12 14:21:14 +0200

    Code Challange W4 src

---
+++ app/src/main/java/com/example/codechallange4/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.codechallange4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewStudents;
+    private List<Student> students;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Code Challange 1
+        //setContentView(R.layout.layout_1);
+
+        //Code Challange 2
+        //setContentView(R.layout.layout_2);
+        //displayStudentsList();
+
+        //Code Challange 2
+        setContentView(R.layout.layout_3);
+    }
+
+    private void inbox() {
+        students = new ArrayList<>();
+        Student student = null;
+        for (int i = 0; i < 25; i++) {
+            student = new Student(0, "FirstName " + i, "LastName " + i);
+            students.add(student);
+        }
+    }
+    private void setStudentsLayoutManager() {
+        recyclerViewStudents = findViewById(R.id.recyclerViewStudents);
+        recyclerViewStudents.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void setStudentsAdapter() {
+        recyclerViewStudents.setAdapter(new StudentAdapter(students,this ));
+    }
+
+    private void displayStudentsList() {
+        // data source
+        inbox();
+
+        // layout manager - checked
+        setStudentsLayoutManager();
+
+        // adapter - checked
+        setStudentsAdapter();
+    }
+}
